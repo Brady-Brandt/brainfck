7299d51b1ff79e706bde508a7c4e97da3961f99b
Brady-Brandt <bradybrandt3@gmail.com>
2025-03-25 20:03:54 -0500

    Added a compiler

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
 #include <stdnoreturn.h>
 #include <stdarg.h>
 #include <ctype.h>
+#include <string.h>
+#include <sys/unistd.h>
 
 
 /*
@@ -19,7 +21,6 @@
 ] 	If the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.[a]
 
 */
-
 
 typedef enum {
     TOK_INCREMENT_DP = '>',
@@ -148,7 +149,7 @@ void check_continous_tokens(FILE* file, char current_char, Tokens* tokens, uint3
     }
     uint32_t total = count + number;
 
-    if(current_char == ','){
+    if(current_char == ',' && count > 1){
         warning("Redunant Use of Instruction ','\nLine %d: Attempting to take user input %d times without incrementing the data pointer\n", line, total);
         total = 0;
     }
@@ -159,45 +160,228 @@ void check_continous_tokens(FILE* file, char current_char, Tokens* tokens, uint3
 }
 
 
-void usage(){
-    fprintf(stderr, "./brainfck {input file}\n");
-}
-
-
-
-
 
 #define DEFAULT_PROGAM_SIZE 1000000
-typedef struct {
-    int8_t* cells;
-    uint32_t dp;
-} Program;
+
+void interpret_progam(uint32_t size, Tokens* tokens){
+    int8_t* cells = calloc(size, 8);
+    if(cells == NULL) fatal_error(OUT_OF_MEM);
+
+    uint32_t dp = 0;    
+    uint32_t ip = 0;
 
 
-Program init_progam(uint32_t size){
-    Program p;
-    p.cells = calloc(size, 8);
-    if(p.cells == NULL) fatal_error(OUT_OF_MEM);
-    p.dp = 0;
-    return p;
+    while(ip < tokens->size){
+        Token tok = tokens->data[ip];
+        switch (tok.type) { 
+            case '>': 
+                dp += tok.amount;
+                break;
+            case '<':
+                dp -= tok.amount;
+                break;
+            case '+':
+                cells[dp] += tok.amount;
+                break;
+            case '-':
+                cells[dp] -= tok.amount;
+                break;
+            case '.':
+                for(int i = 0; i < tok.amount; i++){
+                    fputc(cells[dp], stdout);
+                }
+                break;
+            case ',':
+                cells[dp] = fgetc(stdin);
+                break;
+            case '[':
+                if(cells[dp] == 0){ 
+                    ip = tok.offset;
+                }
+                break;
+            case ']':
+                if(cells[dp] != 0){
+                    ip = tok.offset;
+                }
+                break;
+            default:     
+                break;
+        }
+        ip++;
+
+    }
+    free(cells);
 }
 
-void delete_program(Program* p){
-    free(p->cells); 
+
+
+
+
+#if defined(_WIN64)
+    void compile_progam(const char* file_name, const char* output_file, uint32_t size, Tokens* tokens){
+        fatal_error("The compiler is not supported for windows yet\n");
+    } 
+#elif defined(__APPLE__) && defined(__MACH__)  || defined(__linux__)
+    void compile_progam(const char* file_name, const char* output_file, uint32_t size, Tokens* tokens){
+        size_t file_name_len = strlen(file_name) + 5;
+        char assembly_file[file_name_len];
+
+        char object_file[file_name_len + 7];
+
+
+        sprintf(assembly_file,"%s.asm", file_name);
+        sprintf(object_file, "%s.o", assembly_file);
+
+     
+        FILE* asm_stream = fopen(assembly_file, "w");
+
+        if(asm_stream == NULL) fatal_error("Failed to create assembly file\n");
+
+
+        #if defined (__APPLE__) && defined (__MACH__)
+            fprintf(asm_stream, "global start\nsection .bss\ncells: resb %d\nsection .text\n", size);
+            fprintf(asm_stream, "print:\nmov rax,0x2000004\nmov rdi, 1\nmov rdx,1\nsyscall\nret\n");
+            fprintf(asm_stream, "input:\nmov rax,0x20000000\nmov rdi, 0\nmov rdx,1\nsyscall\nret\n");
+            fprintf(asm_stream, "start:\nmov r13,0\n");
+        #else
+            fprintf(asm_stream, "global _start\nsection .bss\ncells: resb %d\nsection .text\n", size);
+            fprintf(asm_stream, "print:\nmov rax,1\nmov rdi, 1\nmov rdx,1\nsyscall\nret\n");
+            fprintf(asm_stream, "input:\nmov rax, 0\nmov rdi, 0\nmov rdx, 1\nsyscall\nret\n");
+            fprintf(asm_stream, "_start:\nmov r13,0\n");
+        #endif
+
+ 
+        for(uint32_t i = 0; i < tokens->size; i++){
+            Token tok = tokens->data[i];
+            switch (tok.type) { 
+                case '>':
+                    fprintf(asm_stream, "add r13, %d\n", tok.amount);
+                    break;
+                case '<':
+                    fprintf(asm_stream, "sub r13, %d\n", tok.amount);
+                    break;
+                case '+':
+                    fprintf(asm_stream, "mov al, byte [cells + r13]\nadd al, %d\nmov byte [cells + r13], al\n", tok.amount);
+                    break;
+                case '-':
+                    fprintf(asm_stream, "mov al, byte [cells + r13]\nsub al, %d\nmov byte [cells + r13], al\n", tok.amount);
+                    break;
+                case '.':
+                    for(int i = 0; i < tok.amount; i++){
+                        fprintf(asm_stream, "lea rsi, [cells + r13]\ncall print\n");
+                    }
+                    break;
+                case ',':
+                    fprintf(asm_stream, "lea rsi, [cells + r13]\ncall input\n");
+                    break;
+                case '[':
+                    fprintf(asm_stream, "cmp byte [cells + r13], 0\nje label%d\nlabel%d:\n", tok.offset, i);
+                    break;
+                case ']':
+                    fprintf(asm_stream, "cmp byte [cells + r13], 0\njne label%d\nlabel%d:\n", tok.offset, i);
+                    break;
+                default:     
+                    break;
+            }
+
+
+        }
+
+        
+        //exit syscall
+        fprintf(asm_stream,"mov rax, 60\nxor rdi,rdi\nsyscall\n");
+        fclose(asm_stream);
+
+        #define BUF_SIZE 512
+        char cmd[BUF_SIZE] = {0};
+
+
+        size_t cmd_len = snprintf(cmd, BUF_SIZE, "nasm -f elf64 %s -o %s", assembly_file, object_file);
+        int ret = system(cmd);
+        if(ret != 0) fatal_error("Failed to execute nasm\n");
+
+        memset(cmd,0, cmd_len + 1);
+
+
+        snprintf(cmd, BUF_SIZE, "ld -s -o %s %s", output_file, object_file);
+
+        ret = system(cmd);
+        if(ret != 0) fatal_error("Failed to execute ld\n");
+
+
+        ret = remove(assembly_file);
+        if(ret != 0) fatal_error("Failed to cleanup: %s\n", assembly_file);
+        ret = remove(object_file);
+        if(ret != 0) fatal_error("Failed to cleanup: %s\n", object_file);
+
+
+    }
+
+
+
+#else
+    void compile_progam(const char* file_name, const char* output_file, uint32_t size, Tokens* tokens){
+        fatal_error("The compiler is not supported for this platform\n");
+    }
+#endif
+
+
+void usage(){
+    fprintf(stderr, "./brainfck {input file}\n");
+    fprintf(stderr, "Flags: \n");
+    fprintf(stderr, "-c, Compiles the progam (Redunant if using -o)\n");
+    fprintf(stderr, "Options: \n");
+    fprintf(stderr, "-o {output file}, Compiles the progam into an executable named {output_name}\n");
 }
+
+
+
 
 
 int main(int argc, char** argv){ 
     if(argc < 2){
-        usage();
+        fprintf(stderr, "./brainfck {input file}\n");
         return 1;
     }
 
+    char* output_name = "a.out";
+    char* file_name = NULL;
 
-    char* file_name = argv[1];
+
+    bool interpret = true;
+
+    int arg_index = 1;
+    while(arg_index < argc){
+        char* arg = argv[arg_index];
+        
+        if(strcmp(arg, "-o") == 0){
+            interpret = false;
+            if(arg_index + 1 == argc){
+                warning("No output file provided but -o flag was passed. Defaulting to %s\n", output_name);
+                break;
+            }
+            arg_index++;
+            output_name = argv[arg_index]; 
+
+        } else if(strcmp(arg, "-c") == 0){
+            interpret = false;
+
+        } else if(strcmp(arg, "--help") == 0){
+            usage();
+            return 1;
+        } else{
+            file_name = arg;
+        }
+
+        arg_index++;
+    }
+
+    if(file_name == NULL) fatal_error("No input file\n");
+     
+
     FILE* stream = fopen(file_name, "r");
     if(stream == NULL){
-        fprintf(stderr,"Failed to open: %s, ", file_name);
+        fprintf(stderr,"Failed to open: %s\n", file_name);
         perror("");
         return 1;
     }
@@ -268,51 +452,12 @@ int main(int argc, char** argv){
 
     if(bracket_stack.size != 0) fatal_error("No Final Closing Bracket\n");
 
-
-
-    Program p = init_progam(DEFAULT_PROGAM_SIZE);
-    
-    uint32_t ip = 0;
-    while(ip < tokens.size){
-        Token tok = tokens.data[ip];
-        switch (tok.type) { 
-            case '>': 
-                p.dp += tok.amount;
-                break;
-            case '<':
-                p.dp -= tok.amount;
-                break;
-            case '+':
-                p.cells[p.dp] += tok.amount;
-                break;
-            case '-':
-                p.cells[p.dp] -= tok.amount;
-                break;
-            case '.':
-                for(int i = 0; i < tok.amount; i++){
-                    fputc(p.cells[p.dp], stdout);
-                }
-                break;
-            case ',':
-                p.cells[p.dp] = fgetc(stdin);
-                break;
-            case '[':
-                if(p.cells[p.dp] == 0){ 
-                    ip = tok.offset;
-                }
-                break;
-            case ']':
-                if(p.cells[p.dp] != 0){
-                    ip = tok.offset;
-                }
-                break;
-            default:     
-                break;
-        }
-        ip++;
-
+    if(interpret){
+        interpret_progam(DEFAULT_PROGAM_SIZE, &tokens); 
     }
+    else{
+        compile_progam(file_name, output_name, DEFAULT_PROGAM_SIZE, &tokens);
+    } 
 
-    delete_program(&p);
     tokens_delete(&tokens);
 }
